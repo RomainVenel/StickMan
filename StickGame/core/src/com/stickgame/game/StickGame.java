dbe1297519fd706842d2d64553cd9a045345823f
MaximeGenevier <maxime.genevier.sio@gmail.com>
2018-10-03 11:15:42 +0200

    Add comment to StickGame.java
    StickMan le retour

--- StickGame/core/src/com/stickgame/game/StickGame.java
+++ StickGame/core/src/com/stickgame/game/StickGame.java
@@ -2,7 +2,11 @@ package com.stickgame.game;
 
 import com.badlogic.gdx.Game;
 import com.stickgame.game.screens.EcranMenuPrincipal;
-
+/**
+ *
+ *    StickMan by Romain Venel and Maxime Genevier
+ *	
+ */
 public class StickGame extends Game {
 
 	@Override
